845f25ed169a86100e3e81b410c1954e14489942
ali tarichi <tarichiali@gmail.com>
2024-08-13 17:25:24 +0300

    items added to register and buttons and input global classes added

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,3 +1,16 @@
+import SectionHeaders from "@/components/layout/SectionHeaders";
+
 export default function RegisterPage() {
-  return <div>register</div>;
+  return (
+    <section>
+      <div className="text-center">
+        <SectionHeaders MainHeader={"Register"} />
+      </div>
+      <form>
+        <input type="email" placeholder="Your Email" />
+        <input type="password" placeholder="Your Password" />
+        <button type="submit">Register</button>
+      </form>
+    </section>
+  );
 }
