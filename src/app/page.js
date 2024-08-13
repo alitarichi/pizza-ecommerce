180337a5fd67712b4bc203cd6fd0f4cc70d640d9
ali tarichi <tarichiali@gmail.com>
2024-08-13 13:44:35 +0300

    added menu frontend schema

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
-import HomeMenu from "@/components/layout/HmeMenu";
+import HomeMenu from "@/components/layout/HomeMenu";
 
 export default function Home() {
   return (
