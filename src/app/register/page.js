7ca3e18ffde7bf2c85b9449af5ce480842f45d82
ali tarichi <tarichiali@gmail.com>
2024-08-13 17:38:50 +0300

    register css added

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,13 +3,15 @@ import SectionHeaders from "@/components/layout/SectionHeaders";
 export default function RegisterPage() {
   return (
     <section>
-      <div className="text-center">
+      <div className="text-center py-8">
         <SectionHeaders MainHeader={"Register"} />
       </div>
-      <form>
+      <form className="block max-w-xs mx-auto">
         <input type="email" placeholder="Your Email" />
         <input type="password" placeholder="Your Password" />
-        <button type="submit">Register</button>
+        <button className="mt-16" type="submit">
+          Register
+        </button>
       </form>
     </section>
   );
