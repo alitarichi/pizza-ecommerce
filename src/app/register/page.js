4ee4c01944af3cbcdf365d76a98024e4b4cd5736
ali tarichi <tarichiali@gmail.com>
2024-08-14 15:07:07 +0300

    next auth added

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,9 +1,10 @@
 import SectionHeaders from "@/components/layout/SectionHeaders";
+import Image from "next/image";
 
 export default function RegisterPage() {
   return (
     <section>
-      <div className="text-center py-8">
+      <div className="text-center py-8 mb-4">
         <SectionHeaders MainHeader={"Register"} />
       </div>
       <form className="block max-w-xs mx-auto">
@@ -11,6 +12,13 @@ export default function RegisterPage() {
         <input type="password" placeholder="Your Password" />
         <button className="mt-16" type="submit">
           Register
+        </button>
+        <div className="my-4 text-center text-gray-500">
+          <h1>or login with provider</h1>
+        </div>
+        <button className="flex gap-4 justify-center">
+          <Image src={"/google.png"} alt="google" width={24} height={24} />
+          Login with Google
         </button>
       </form>
     </section>
