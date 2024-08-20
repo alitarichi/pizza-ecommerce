a6c86b5328c26bda0ba00c308db1a98b4e55ab98
ali tarichi <tarichiali@gmail.com>
2024-08-20 17:45:26 +0300

    not sure commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "*.googleusercontent.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -2,6 +2,7 @@
 
 import SectionHeaders from "@/components/layout/SectionHeaders";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default function ProfilePage() {
@@ -13,15 +14,22 @@ export default function ProfilePage() {
     return "loading...";
   }
 
-  if (!status) {
+  if (status === "unauthenticated") {
     return redirect("/login");
   }
+
+  const userImage = session.user?.Image;
 
   return (
     <section>
       <div className="text-center py-8 mb-4">
         <SectionHeaders MainHeader={"Profile"} />
       </div>
+      <form className="max-w-xs mx-auto border">
+        <div>
+          <Image src={userImage} alt="avatar" width={64} height={64} />
+        </div>
+      </form>
     </section>
   );
 }
