9ca078b2bb03df8168882d688665e9c58c38d6c8
ali tarichi <tarichiali@gmail.com>
2024-08-21 15:56:26 +0300

    fixed some typos

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 
-export async function PUT() {
-  mongoose.connect(process.env.MONGODB_URL);
-  const data = await requestAnimationFrame.json();
+export async function PUT(req) {
+  mongoose.connect(process.env.MONGO_URL);
+  const data = await req.json();
   const session = await getServerSession(authOptions);
+  console.log(session);
   if ("name" in data) {
   }
 }
