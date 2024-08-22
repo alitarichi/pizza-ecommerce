ec9d26d7e52995bbf089cdb8b9c49f1cfc26d8c6
ali tarichi <tarichiali@gmail.com>
2024-08-22 17:32:23 +0300

    upload file successfully

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,3 +1,7 @@
-export function POST(req) {
+export async function POST(req) {
+  const data = await req.formData();
+  if (data.get("file")) {
+    console.log("we have file", data.get("file"));
+  }
   return Response.json(true);
 }
