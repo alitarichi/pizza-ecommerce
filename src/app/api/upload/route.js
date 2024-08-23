8e247fe824fc2fa8d5e40b8f80cd80b48193f73f
ali tarichi <tarichiali@gmail.com>
2024-08-23 16:34:49 +0300

    started working on upload photos

--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,7 +1,7 @@
 export async function POST(req) {
   const data = await req.formData();
   if (data.get("file")) {
-    console.log("we have file", data.get("file"));
+    // upload the file
   }
   return Response.json(true);
 }
